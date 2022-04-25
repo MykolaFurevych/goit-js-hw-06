9fcc25efe7b140ca9f8378cdf3d7a5845198c10d
furev.mykola <aleniki21@gmail.com>
2022-04-25 17:52:48 +0300

    Змінив код

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,12 @@
 
 
 const inputEl = document.querySelector('#validation-input');
-    inputEl.addEventListener('blur', function () {
-  if (this.dataset.length === this.value.length) {
+ inputEl.addEventListener('blur', function () {
+  if (Number(this.dataset.length) === this.value.length) {
     this.classList.add('valid');
     this.classList.remove('invalid');
   } else {
     this.classList.add('invalid');
   };
 });
+
