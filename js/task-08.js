3933910c5aa3767d0edf0ca7311e91ee93232d13
furev.mykola <aleniki21@gmail.com>
2022-05-01 21:46:29 +0300

    змінив код

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,12 @@
-
-
 const formEl = document.querySelector(".login-form");
-formEl.addEventListener('submit', event => {
+formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  const  email = formEl.elements.email.value;
-  const  password = formEl.elements.password.value;
-    if (email === '' || password === '') {
-        alert('Всі поля мають бути заповнені!');
-    }
-    else {
-        console.log({email, password});
-    }
-       event.target.reset();
+  const email = formEl.elements.email.value;
+  const password = formEl.elements.password.value;
+  if (email === "" || password === "") {
+    alert("Всі поля мають бути заповнені!");
+  } else {
+    console.log({ email, password });
+    event.target.reset();
+  }
 });
-
